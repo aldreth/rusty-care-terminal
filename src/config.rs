1d43896c312b8cae0339817b0ff6df3cd0c3b2e6
Edward Andrews-Hodgson <edward.andrewshodgson@anaplan.com>
2022-11-08 00:30:19 +0000

    That's better - don't have to push into a Vec

--- src/config.rs
+++ src/config.rs
@@ -3,20 +3,15 @@ use std::env;
 use std::fs;
 
 pub fn get_directories() -> Vec<fs::DirEntry> {
-    let mut dirs: Vec<fs::DirEntry> = Vec::new();
-
     env::var("TTC_REPOS")
         .expect("TTC_REPOS environment variable is not set")
         .split(',')
-        .for_each(|dir| {
+        .flat_map(|dir| {
             fs::read_dir(dir)
                 .expect("Directory from TTC_REPOS not found")
                 .flatten()
                 .filter(|x| !x.path().is_file())
                 .filter(|x| is_git(x.path().join(".git")).is_ok())
-                .for_each(|y| {
-                    dirs.push(y);
-                })
-        });
-    dirs
+        })
+        .collect()
 }
