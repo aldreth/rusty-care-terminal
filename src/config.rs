95b0139fd5a4ce0d86af4152c4f86aeafd721500
Edward Andrews-Hodgson <edward.andrewshodgson@anaplan.com>
2022-12-07 16:58:36 +0000

    Use RCT env var

--- src/config.rs
+++ src/config.rs
@@ -3,12 +3,12 @@ use std::env;
 use std::fs;
 
 pub fn get_directories() -> Vec<fs::DirEntry> {
-    env::var("TTC_REPOS")
-        .expect("TTC_REPOS environment variable is not set")
+    env::var("RCT_REPOS")
+        .expect("RCT_REPOS environment variable is not set")
         .split(',')
         .flat_map(|dir| {
             fs::read_dir(dir)
-                .expect("Directory from TTC_REPOS not found")
+                .expect("Directory from RCT_REPOS not found")
                 .flatten()
                 .filter(|x| !x.path().is_file())
                 .filter(|x| is_git(x.path().join(".git")).is_ok())
